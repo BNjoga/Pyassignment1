77b46a5230087be5a798ef78d6af2cef074a38f0
BNjoga <njogabrian30@gmail.com>
2025-08-04 08:20:45 +0300

    py assignment 1 file

---
+++ lesson1.py
@@ -0,0 +1,14 @@
+#Ask the use to input two numbers and operations
+num1 = float(input("enter the first number:"))
+num2 = float(input("enter the second number:"))
+#perform the operation
+sum_result = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
+quotient_result = num1 / num2
+#display the results
+print(f"Results of your two numbers:")
+print(f"Sum: {sum_result}")
+print(f"Difference: {difference_result}")
+print(f"Product: {product_result}")
+print(f"Quotient: {quotient_result}")
